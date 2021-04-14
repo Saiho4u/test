cce55508e70e9f328ab2a99532076c7b232259df
liangwei <banemail@gmail.com>
2021-04-14 16:04:38 +0800

    定义了一个自定义指令

--- src/main.js
+++ src/main.js
@@ -35,6 +35,21 @@ Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
+// 添加自定义指令
+// Vue.directive(指令名, 指令的配置对象)
+Vue.directive('imgerror', {
+  // 这里配置一些钩子函数
+  // 这里的钩子,是指那些被使用当前指令的 dom 的声明周期
+  inserted(dom, options) {
+    // 这个钩子会在 dom 被插入父组件的时候触发,
+    // 可以接受到两个参数
+    // 1. 当前的 dom
+    // 2. 当前指令的配置
+    console.log(dom)
+    // 接下来只需要定义他的错误处理, 修改 src 即可
+  }
+})
+
 new Vue({
   el: '#app',
   router,
