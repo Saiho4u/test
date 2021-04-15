47338f703b1ef2b311f5057a223bb1c066534a40
liangwei <banemail@gmail.com>
2021-04-15 15:09:47 +0800

    17-修改部门树形数据的布局样式

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -37,7 +37,35 @@
           <!-- 每个插槽都有两个参数, 第一插槽名, 第二作用域插槽形参 -->
           <template slot-scope="scope">
             <!-- 每次渲染到的部门数据, 就会在 scope.data 里面储存 -->
-            {{ scope.data.name }}
+            <!-- {{ scope.data.name }} -->
+            <el-row
+              type="flex"
+              justify="space-between"
+              align="middle"
+              style="height: 40px; width: 100%"
+            >
+              <el-col>
+                <span>{{ scope.data.name }}</span>
+              </el-col>
+              <el-col :span="4">
+                <el-row type="flex" justify="end">
+                  <!-- 两个内容 -->
+                  <el-col>{{ scope.data.manager }}</el-col>
+                  <el-col>
+                    <!-- 下拉菜单 element -->
+                    <el-dropdown>
+                      <span> 操作<i class="el-icon-arrow-down" /> </span>
+                      <!-- 下拉菜单 -->
+                      <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>添加子部门</el-dropdown-item>
+                        <el-dropdown-item>编辑部门</el-dropdown-item>
+                        <el-dropdown-item>删除部门</el-dropdown-item>
+                      </el-dropdown-menu>
+                    </el-dropdown>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
           </template>
         </el-tree>
       </el-card>
@@ -50,9 +78,21 @@ export default {
   data() {
     return {
       departs: [
-        { name: '总裁办', children: [{ name: '董事会' }] },
-        { name: '行政部' },
-        { name: '人事部' }
+        {
+          name: '总裁办',
+          manager: '老王',
+          children: [
+            { name: '董事会', manager: '小王' }
+          ]
+        },
+        {
+          name: '行政部',
+          manager: '大王'
+        },
+        {
+          name: '人事部',
+          manager: '炸弹'
+        }
       ],
       treeOption: {
         label: 'name'
