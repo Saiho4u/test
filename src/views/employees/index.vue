34ca21b519e14e6fa57f17dabd8fb3010939715f
liangwei <banemail@gmail.com>
2021-04-19 09:15:24 +0800

    04-员工页面布局

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <!-- 3. 渲染 -->
-      <PageTools :show-before="true">
-        <template slot="before">
-          123
-        </template>
+      <!-- 这里是头部公共工具栏 -->
+      <page-tools :show-before="true">
+        <span slot="before">共166条记录</span>
         <template slot="after">
-          456
+          <el-button size="small" type="warning">导入</el-button>
+          <el-button size="small" type="danger">导出</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
         </template>
-      </PageTools>
+      </page-tools>
+      <!-- 页面主要内容 -->
+      <el-card>
+        <el-table border>
+          <el-table-column label="序号" sortable="" />
+          <el-table-column label="姓名" sortable="" />
+          <el-table-column label="工号" sortable="" />
+          <el-table-column label="聘用形式" sortable="" />
+          <el-table-column label="部门" sortable="" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="账户状态" sortable="" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row type="flex" justify="center" align="middle" style="height: 60px">
+          <el-pagination layout="prev, pager, next" />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
