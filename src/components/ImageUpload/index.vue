d71301fece67829d1ed79bc6826e2df03bd3484a
liangwei <banemail@gmail.com>
2021-04-22 15:55:50 +0800

    上传组件必填属性

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,10 +1,35 @@
 <template>
-  <div>上传组件</div>
+  <div>
+    <!--
+      action 必填, 但是后面我们用腾讯云所以就放一个 # 号即可
+      list-type="picture-card" 决定样式
+
+      file-list 是用来管理当前文件列表的, 是一个数组
+     -->
+    <el-upload
+      action="#"
+      list-type="picture-card"
+      :file-list="fileList"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      fileList: [
+        // 这里是存放文件列表的地方
+        // 其中每张图片都是一个对象
+        // 最重要的属性 url
+        {
+          url: 'https://img1.baidu.com/it/u=145965533,3075282827&fm=26&fmt=auto&gp=0.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
 
