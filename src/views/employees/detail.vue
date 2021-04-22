2c79f7ebe53d3ac0f544dc46440e2ba4b2ca811d
liangwei <banemail@gmail.com>
2021-04-22 10:46:54 +0800

    创建员工个人详情组件

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -6,20 +6,24 @@
           <el-tab-pane label="登录账户设置">
 
             <!-- 放置表单 -->
-            <el-form label-width="120px" style="margin-left: 120px; margin-top:30px">
-              <el-form-item label="姓名:">
+            <el-form ref="formData" :model="formData" :rules="rules" label-width="120px" style="margin-left: 120px; margin-top:30px">
+              <el-form-item label="姓名:" prop="username">
                 <el-input v-model="formData.username" style="width:300px" />
               </el-form-item>
-              <el-form-item label="密码:">
+              <el-form-item label="密码:" prop="password2">
                 <el-input v-model="formData.password2" style="width:300px" type="password" />
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
+                <el-button type="primary" @click="btnOK">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
-          <el-tab-pane label="个人详情" />
-          <el-tab-pane label="岗位信息" />
+          <el-tab-pane label="个人详情">
+            <UserInfo />
+          </el-tab-pane>
+          <el-tab-pane label="岗位信息">
+            456456
+          </el-tab-pane>
         </el-tabs>
       </el-card>
     </div>
@@ -28,13 +32,28 @@
 
 <script>
 import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
+import UserInfo from './components/user-info'
 export default {
+  components: {
+    UserInfo
+  },
   data() {
     return {
       formData: {
         username: '',
         // password: ''
         password2: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '改数据不能为空', trigger: 'blur' },
+          { min: 1, max: 5, message: '长度在 1-5 之间', trigger: 'blur' }
+        ],
+        password2: [
+          { required: true, message: '改数据不能为空', trigger: 'blur' },
+          { min: 6, max: 16, message: '长度在 6-16 之间', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -44,6 +63,11 @@ export default {
   methods: {
     async getUserDetailById() {
       this.formData = await getUserDetailById(this.$route.params.id)
+    },
+    async btnOK() {
+      await this.$refs.formData.validate()
+      await saveUserDetailById({ ...this.formData, password: this.formData.password2 })
+      this.$message.success('操作成功')
     }
   }
 }
