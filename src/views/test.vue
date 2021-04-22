569ac26a0b02bc4d69a85ca0fa6b54ffdadd715a
liangwei <banemail@gmail.com>
2021-04-22 11:06:49 +0800

    演示动态组件的使用方法

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>测试页</div>
+  <div>
+    <!-- 动态组件的使用方法
+    动态组件可以用来显示任意可用组件 -->
+    <!-- 只需要绑定一个 :is 属性, 指定对应的组件名即可 -->
+    <button @click="componentName = 'UserInfo'">个人信息</button>
+    <button @click="componentName = 'JobInfo'">岗位信息</button>
+    <br>
+    <component :is="componentName" />
+    <!-- <UserInfo v-if="componentName === 'UserInfo'" />
+    <JobInfo v-if="componentName === 'JobInfo'" /> -->
+  </div>
 </template>
 
 <script>
+import UserInfo from '@/views/employees/components/user-info'
+import JobInfo from '@/views/employees/components/job-info'
 export default {
-
+  components: {
+    UserInfo,
+    JobInfo
+  },
+  data() {
+    return {
+      componentName: 'JobInfo'
+    }
+  }
 }
 </script>
 
