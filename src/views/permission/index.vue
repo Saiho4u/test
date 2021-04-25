551af19be346af77e3f6a6f586b97077d857ce81
liangwei <banemail@gmail.com>
2021-04-25 15:00:06 +0800

    15-创建权限页面 初步渲染表格数据

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,16 +1,47 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        权限
-      </h2>
+      <!-- 靠右的按钮 -->
+      <page-tools>
+        <template v-slot:after>
+          <el-button type="primary" size="small">添加权限</el-button>
+        </template>
+      </page-tools>
+      <!-- 表格 -->
+      <el-table border :data="permissionList">
+        <el-table-column align="center" label="名称" prop="name" />
+        <el-table-column align="center" label="标识" prop="code" />
+        <el-table-column align="center" label="描述" prop="description" />
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">添加</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
 export default {
-
+  data() {
+    return {
+      permissionList: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async getPermissionList() {
+      const res = await getPermissionList()
+      console.log(res)
+      this.permissionList = res
+    }
+  }
 }
 </script>
 
