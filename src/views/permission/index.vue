3f86f00a92bb120dc935a27d49de3d75ec380926
liangwei <banemail@gmail.com>
2021-04-25 15:55:30 +0800

    18-添加权限弹窗完毕

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -4,7 +4,7 @@
       <!-- 靠右的按钮 -->
       <page-tools>
         <template v-slot:after>
-          <el-button type="primary" size="small">添加权限</el-button>
+          <el-button type="primary" size="small" @click="addPerm">添加权限</el-button>
         </template>
       </page-tools>
       <!-- 表格 -->
@@ -14,12 +14,40 @@
         <el-table-column align="center" label="描述" prop="description" />
         <el-table-column align="center" label="操作">
           <template>
-            <el-button type="text">添加</el-button>
+            <el-button type="text" @click="addPerm">添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
+
+      <el-dialog :title="`${showText}权限点`" :visible="showDialog" @close="btnCancel">
+        <!-- 表单 -->
+        <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+          <el-form-item label="权限名称" prop="name">
+            <el-input v-model="formData.name" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限标识" prop="code">
+            <el-input v-model="formData.code" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%" />
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch
+              v-model="formData.enVisible"
+              active-value="1"
+              inactive-value="0"
+            />
+          </el-form-item>
+        </el-form>
+        <el-row slot="footer" type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+            <el-button size="small" @click="btnCancel">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -31,7 +59,23 @@ import { listToTreeData } from '@/utils'
 export default {
   data() {
     return {
-      permissionList: []
+      permissionList: [],
+
+      showDialog: false,
+      formData: {
+        name: '',
+        code: '',
+        description: '',
+        enVisible: '1'
+      },
+      rules: {
+
+      }
+    }
+  },
+  computed: {
+    showText() {
+      return this.formData.id ? '编辑' : '新增'
     }
   },
   created() {
@@ -45,6 +89,11 @@ export default {
       this.permissionList = listToTreeData(res, '0')
       console.log('列表转换后')
       console.log(this.permissionList)
+    },
+    btnOK() {},
+    btnCancel() {},
+    addPerm() {
+      this.showDialog = true
     }
   }
 }
