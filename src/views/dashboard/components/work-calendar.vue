bba32d60a1edf0d8c2e8506fae78e4c2a2fd9e3e
liangwei <banemail@gmail.com>
2021-04-28 12:00:49 +0800

    10-自定义日历的显示格式

--- src/views/dashboard/components/work-calendar.vue
+++ src/views/dashboard/components/work-calendar.vue
@@ -9,18 +9,28 @@
       </el-select>
     </el-row>
     <el-calendar v-model="currentDate">
-      <!-- <template v-slot:dateCell="{ date, data }" class="content">
+      <!-- <template slot="dateCell" slot-scope="scope">
+        <div>{{ scope.data.day.split('-')[2] }}</div>
+      </template> -->
+      <template v-slot:dateCell="{ date, data }" class="content">
         <div class="date-content">
           <span class="text"> {{ data.day | getDay }}</span>
-          <span v-if="isWeek(date)" class="rest">休</span>
+          <span v-if="isWeekend(date)" class="rest">休</span>
+          <!-- <span v-if="isWeek(date)" class="rest">休</span> -->
         </div>
-      </template> -->
+      </template>
     </el-calendar>
   </div>
 </template>
 
 <script>
 export default {
+  filters: {
+    getDay(oldVal) {
+      const dayStr = oldVal.split('-')[2]
+      return dayStr.startsWith('0') ? dayStr.substr(1) : dayStr
+    }
+  },
   data() {
     return {
       currentDate: new Date(),
@@ -48,11 +58,56 @@ export default {
       const year = this.currentYear
       const month = this.currentMonth
       this.currentDate = new Date(`${year}-${month}-1`)
+    },
+    isWeekend(date) {
+      // 获取一个日期星期几的方法 getDay
+      // 这里只判断星期六星期天
+      // 如果需要其他节假日
+      // 需要后端的配合传来一个节假日的数组
+      // 我们就可以判断当前日期是否在数组中
+      return date.getDay() === 6 || date.getDay() === 0
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss"  scoped>
+ ::v-deep .el-calendar-day {
+  height:  auto;
+ }
+ ::v-deep .el-calendar-table__row td,::v-deep .el-calendar-table tr td:first-child,  ::v-deep .el-calendar-table__row td.prev{
+  border:none;
+ }
+.date-content {
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  font-size: 14px;
+}
+.date-content .rest {
+  color: #fff;
+  border-radius: 50%;
+  background: rgb(250, 124, 77);
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  display: inline-block;
+  font-size: 12px;
+  margin-left: 10px;
+}
+.date-content .text{
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+ display: inline-block;
 
+}
+::v-deep .el-calendar-table td.is-selected .text{
+   background: #409eff;
+   color: #fff;
+   border-radius: 50%;
+ }
+::v-deep .el-calendar__header {
+   display: none
+ }
 </style>
