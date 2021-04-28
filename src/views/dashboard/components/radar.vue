2295c4309787e49f9b6d7adb1af34c7bbbbd1ab8
liangwei <banemail@gmail.com>
2021-04-28 15:14:06 +0800

    14-雷达图实现

--- src/views/dashboard/components/radar.vue
+++ src/views/dashboard/components/radar.vue
@@ -16,18 +16,34 @@ export default {
     var myChart = echarts.init(this.$refs.myChart)
     // 3. 绘制图表 setOption 并传入配置对象
     myChart.setOption({
-      title: {
-        text: 'ECharts 入门示例'
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: '销售（Sales）', max: 6500 },
+          { name: '管理（Administration）', max: 16000 },
+          { name: '信息技术（Information Technology）', max: 30000 },
+          { name: '客服（Customer Support）', max: 38000 },
+          { name: '研发（Development）', max: 52000 },
+          { name: '市场（Marketing）', max: 25000 }
+        ]
       },
-      tooltip: {},
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
+      color: ['#ee6666', '#73c0de', '#3ba272', '#fc8452', '#9a60b4', '#ea7ccc', '#5470c6', '#91cc75', '#fac858'],
       series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
+        type: 'radar',
+        data: [
+          {
+            value: [4200, 200, 20000, 35000, 50000, 18000],
+            name: '预算分配（Allocated Budget）'
+          },
+          {
+            value: [5000, 14000, 28000, 26000, 42000, 21000],
+            name: '实际开销（Actual Spending）'
+          },
+          {
+            value: [2000, 15000, 18000, 6000, 3000, 11000],
+            name: '测试'
+          }
+        ]
       }]
     })
   }
