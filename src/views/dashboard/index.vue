4fe3431d35ee8db7241d20fa66ee2a12f1d9bdb9
liangwei <banemail@gmail.com>
2021-04-28 15:51:17 +0800

    添加审批弹窗

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -72,7 +72,7 @@
             <span>流程申请</span>
           </div>
           <div class="sideNav">
-            <el-button class="sideBtn">申请离职</el-button>
+            <el-button class="sideBtn" @click="showDialog = true">申请离职</el-button>
             <el-button class="sideBtn">请假调休</el-button>
             <el-button class="sideBtn">审批列表</el-button>
             <el-button class="sideBtn">我的信息</el-button>
@@ -117,6 +117,42 @@
         </el-card>
       </el-col>
     </el-row>
+
+    <!-- 弹出层 -->
+    <el-dialog :visible="showDialog" title="离职申请" @close="btnCancel">
+      <el-form
+        ref="ruleForm"
+        :model="ruleForm"
+        status-icon
+        label-width="110px"
+        :rules="rules"
+      >
+        <!--离职表单-->
+        <el-form-item label="离职时间" prop="end_time">
+          <el-date-picker
+            v-model="ruleForm.exceptTime"
+            type="datetime"
+            value-format="yyyy-MM-dd HH:mm:ss"
+            placeholder="选择日期时间"
+          />
+        </el-form-item>
+        <el-form-item label="离职原因" prop="reason">
+          <el-input
+            v-model="ruleForm.reason"
+            type="textarea"
+            :autosize="{ minRows: 3, maxRows: 8}"
+            style="width: 70%;"
+            placeholder="请输入内容"
+          />
+        </el-form-item>
+      </el-form>
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -144,6 +180,30 @@ export default {
     ...mapState([
       'userInfo'
     ])
+  },
+  data() {
+    return {
+      showDialog: false,
+      ruleForm: {
+        exceptTime: '',
+        reason: '',
+        processKey: 'process_dimission', // 特定的审批
+        processName: '离职'
+      },
+      rules: {
+        exceptTime: [{ required: true, message: '离职时间不能为空' }],
+        reason: [{ required: true, message: '离职原因不能为空' }]
+      }
+    }
+  },
+  methods: {
+    btnOK() {
+      // 表单验证
+      // 发请求
+      // 提示用户
+      // 关闭弹窗
+    },
+    btnCancel() {}
   }
   // mounted() {
   //   this.$store.dispatch('user/getUserInfo')
