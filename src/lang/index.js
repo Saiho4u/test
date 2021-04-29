b9c960a3909d53ad465960dcaa4695a3b7878023
liangwei <banemail@gmail.com>
2021-04-29 11:11:02 +0800

    09-国际化插件饿了么部分实现

--- src/lang/index.js
+++ src/lang/index.js
@@ -3,20 +3,26 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 // 2. Vue.use 注册
 Vue.use(VueI18n)
+
+// 引入饿了么的翻译字典
+import elementZH from 'element-ui/lib/locale/lang/zh-CN'
+import elementEN from 'element-ui/lib/locale/lang/en'
+
 // 3. 创建实例
 export default new VueI18n({
   // 当前语言设定
-  locale: 'zh',
+  locale: 'en',
   // 字典
   messages: {
     zh: {
       hi: '你好',
-      goodday: '祝你开心每一天'
-
+      goodday: '祝你开心每一天',
+      ...elementZH
     },
     en: {
       hi: 'Hello',
-      goodday: 'Have a good day'
+      goodday: 'Have a good day',
+      ...elementEN
     }
   }
 })
